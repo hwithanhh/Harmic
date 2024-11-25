dbd0590527fd2f30a88ca39aa7f4159bb188f13a
Baong <Baong@THANHHOAI>
2024-11-25 15:21:05 +0700

    TH5-LỖI

---
+++ Test_Harmic/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Test_Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_Harmic.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+        public ProductViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbProducts.Include(m => m.CategoryProduct).Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+            return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m => m.ProductId).ToList()));
+        }
+    }
+}
