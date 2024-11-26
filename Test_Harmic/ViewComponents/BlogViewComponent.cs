1d45fb904a72b6b4745c5fea863314cf73ab004f
Baong <Baong@THANHHOAI>
2024-11-26 16:22:11 +0700

    Bài TH5 - HC

---
+++ Test_Harmic/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Test_Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_Harmic.ViewComponents
+{
+    public class BlogViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+        public BlogViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var blogs = await _context.TbBlogs
+                        .Where(b => b.IsActive) // Lọc các bài viết đang hoạt động
+                        .OrderByDescending(b => b.CreatedDate) // Sắp xếp theo ngày tạo giảm dần
+                        .ToListAsync(); // Thực hiện truy vấn bất đồng bộ
+
+            return View(blogs); // Trả về view với danh sách bài viết
+        }
+    }
+}
